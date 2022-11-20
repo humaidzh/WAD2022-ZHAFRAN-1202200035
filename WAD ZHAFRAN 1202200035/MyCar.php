6cbc8415f4a552755d8d87e0c89fe182acfdf8d5
humaidzh <115598322+humaidzh@users.noreply.github.com>
2022-11-20 18:09:45 +0700

    Add files via upload

---
+++ WAD ZHAFRAN 1202200035/MyCar.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Tambah Mobil">
+    <meta name="description" content="">
+    <title>MyCar</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="MyCar.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.20.1, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    
+    
+    
+   
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="MyCar">
+    <meta property="og:type" content="website">
+  </head>
+
+        </a>
+        <nav class="u-menu u-menu-mega u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px; font-weight: 700;">
+            <a class="u-button-style u-custom-active-border-color u-custom-border u-custom-border-color u-custom-borders u-custom-hover-border-color u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-decoration u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-spacing-30 u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-palette-1-base" href="Home.html" style="padding: 20px 5px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-palette-1-base" href="#" style="padding: 20px 5px;">MyCar</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="#">MyCar</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-clearfix u-gradient u-section-1" id="sec-7a4c">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-default u-text-1">Tambah Mobil</h2>
+        <div class="u-form u-form-1">
+          <form action="https://forms.nicepagesrv.com/Form/Process" class="u-clearfix u-form-spacing-15 u-form-vertical u-inner-form" style="padding: 15px;" source="email" name="form">
+            <div class="u-form-group u-form-name u-label-none">
+              <label for="name-6797" class="u-label">Nama Mobil</label>
+              <input type="text" placeholder="Nama Mobil" id="name-6797" name="Nama Mobil" class="u-border-1 u-border-grey-30 u-input u-input-rectangle" required="">
+            </div>
+            <div class="u-form-email u-form-group u-form-partition-factor-2 u-label-none">
+              <label for="email-6797" class="u-label">Email</label>
+              <input type="email" placeholder="Nama Pemilik" id="email-6797" name="Nama Pemilik" class="u-border-1 u-border-grey-30 u-input u-input-rectangle" required="">
+            </div>
+            <div class="u-form-group u-label-none">
+              <label for="text-62e7" class="u-label">Input</label>
+              <input type="text" placeholder="Merk" id="text-62e7" name="Merk" class="u-border-1 u-border-grey-30 u-input u-input-rectangle">
+            </div>
+            <div class="u-form-group u-label-none">
+              <label for="text-7737" class="u-label">Input</label>
+              <input type="text" placeholder="Tanggal Beli" id="text-7737" name="Tanggal Beli" class="u-border-1 u-border-grey-30 u-input u-input-rectangle">
+            </div>
+            <div class="u-form-group u-form-message u-label-none">
+              <label for="message-6797" class="u-label">Deskripsi</label>
+              <textarea placeholder="Deskripsi" rows="4" cols="50" id="message-6797" name="Deskripsi" class="u-border-1 u-border-grey-30 u-input u-input-rectangle" required=""></textarea>
+            </div>
+            <div class="u-form-checkbox u-form-group u-label-none">
+              <input type="checkbox" id="checkbox-2243" name="Lunas" value="On" accept="Lunas">
+              <label for="checkbox-2243" class="u-label">Lunas</label>
+            </div>
+            <div class="u-form-checkbox u-form-group u-label-none">
+              <input type="checkbox" id="checkbox-02e6" name="checkbox-1" value="On">
+              <label for="checkbox-02e6" class="u-label">Belum Lunas</label>
+            </div>
+            <div class="u-align-left u-form-group u-form-submit">
+              <a href="#" class="u-btn u-btn-submit u-button-style">Selesai</a>
+              <input type="submit" value="submit" class="u-form-control-hidden">
+            </div>
+            <div class="u-form-send-message u-form-send-success">Thank you! Your message has been sent.</div>
+            <div class="u-form-send-error u-form-send-message">Unable to send your message. Please fix errors then try again.</div>
+            <input type="hidden" value="" name="recaptchaResponse">
+            <input type="hidden" name="formServices" value="36bc68f9aa8627db8d3355d5f2c621c0">
+          </form>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-section-2" id="sec-7a60">
+      <div class="u-clearfix u-sheet u-sheet-1"></div>
+    </section>
+    <section class="u-align-center u-clearfix u-section-3" id="sec-b6d4">
+      <div class="u-clearfix u-sheet u-sheet-1"></div>
+    </section>
+    <section class="u-align-center u-clearfix u-section-4" id="sec-d15b">
+      <div class="u-clearfix u-sheet u-sheet-1"></div>
+    </section>
+    
+    
+  
+  
+</body></html>
